e695bc16f7479420cb2fa7d6a4ddfbe77501290d
Nathan Binks <FoulPlay@users.noreply.github.com>
2016-08-19 15:21:01 +0100

    Update 1 (Pre-Alpha)
    Small update to add and fix comments.

--- sent_rp_slotmachine.lua
+++ sent_rp_slotmachine.lua
@@ -1,4 +1,4 @@
---[[rp_slotmachine by Foul Play | Version 1.0.0 Pre-Alpha]]
+--[[rp_slotmachine by Foul Play | Version 1.0.1 Pre-Alpha]]
 --[[
 	function() end --A function
 	for() do --A loop
@@ -7,6 +7,12 @@
 	else --A condiction
 	elseif then --A condiction
 	a = nil --A var
+	a ~= b -- Relational Operator
+	a == b --Relational Operator
+	a > b -- Relational Operator
+	a < b -- Relational Operator
+	a => b -- Relational Operator
+	a <= b -- Relational Operator
 	a + b = c --Add maths
 	a - b = c --Subtract maths
 	a / b = c --Divide maths
@@ -17,19 +23,19 @@ AddCSLuaFile()
 
 DEFINE_BASECLASS( "base_anim" )
 
-cleanup.Register( "" )
+cleanup.Register( "" ) --Registers for the entity to be cleaned up by Admins and clients.
 
-ENT.PrintName = ""
-ENT.Author = "Nathan Binks"
-ENT.Information = ""
-ENT.Category = ""
-ENT.Spawnable = true
-ENT.AdminOnly = false
-ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
+ENT.PrintName = "" --The name of the entity in the Spawn Menu.
+ENT.Author = "Nathan Binks" --The author of the entity in the Spawn Menu.
+ENT.Information = "" --Information about the entity in the Spawn Menu.
+ENT.Category = "" --The Category where its going to be stored in the Spawn Menu.
+ENT.Spawnable = true --Makes it Spawnable by Clients.
+ENT.AdminOnly = false --Make it so non-admins can spawn it in.
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT --TODO: Add information about what it does.
 
 function ENT:SetupDataTables()
-	self:NetworkVar( "Int", 0, "price" )
-	self:NetworkVar( "Entity", 1, "owning_ent" )
+	self:NetworkVar( "Int", 0, "price" ) --The price of the entity in the F4 Menu in DarkRP.
+	self:NetworkVar( "Entity", 1, "owning_ent" ) --Sets the owner of the entity when spawned by clients to them.
 end
 
 function ENT:SpawnFunction( ply, tr, ClassName )
@@ -42,23 +48,22 @@ function ENT:SpawnFunction( ply, tr, ClassName )
 	ent:Spawn() --Spawn the entity.
 	ent:Activate() --Activate the entity.
 	ent:PhysWake() --Makes the entity fall to the ground.
-	
-	ply:AddCleanup( "", ent )
+
+	ply:AddCleanup( "", ent ) --Adds the entity that the client has spawn to their cleanup menu in Spawn Menu.
 
 	return ent --Return the entity.
 end
 
 function ENT:Initialize()
-	--Set the model ingame.
-	self:SetModel( "" )
+	self:SetModel( "" ) --Set the model ingame.
 
 	--Enables Physics on Client.
 	self:SetMoveType( MOVETYPE_VPHYSICS )
 	self:SetSolid( SOLID_VPHYSICS )
 
 	if ( SERVER ) then
-		--Only use this Physics on server side or the Physgun beam will fuck up.
-		self:PhysicsInit( SOLID_VPHYSICS )
+		--Only use this on server side because this is server side only Physics or else the PhysGun beam will be set incorrectly.
+		self:PhysicsInit( SOLID_VPHYSICS ) 
 	end
 end
 
@@ -74,10 +79,9 @@ end
 --https://github.com/garrynewman/garrysmod/blob/master/garrysmod/lua/entities/sent_ball.lua#L149
 if ( SERVER ) then return end -- We do NOT want to execute anything below in this FILE on SERVER 
 
-language.Add( "Cleanup_", "" )
-language.Add( "Cleaned_", "" )
+language.Add( "Cleanup_", "" ) --Sets what it says in the cleanup menu in the Spawn Menu.
+language.Add( "Cleaned_", "" ) --Sets what it says when the entity gets cleaned.
 
 function ENT:Draw()
-	--Drawing the model
-	self:DrawModel()
+	self:DrawModel() --Drawing the model
 end
